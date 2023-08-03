e993e3af656ea95385225fc77d0b28c260172f59
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-08-03 11:22:30 +0700

    create footer Widget and integrate into main

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -13,6 +13,7 @@ use yii\bootstrap5\NavBar;
 use frontend\widgets\leftMenuWidget;
 use frontend\widgets\topProductWidget;
 use frontend\widgets\bestSellerProductWidget;
+use frontend\widgets\footerWidget;
 
 AppAsset::register($this);
 ?>
@@ -456,52 +457,12 @@ AppAsset::register($this);
         </div>
     </div>
     <!-- Wrapper END -->
+
     <!-- Footer -->
-    <footer class="iq-footer">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-9">
-                    <ul class="list-inline mb-0">
-                        <li class="list-inline-item"><a href="privacy-policy.html">Privacy Policy</a></li>
-                        <li class="list-inline-item"><a href="terms-of-service.html">Terms of Use</a>
-                        <li class="list-inline-item">
-                            <!-- Facebook -->
-                            <a type="button" class="btn btn-floating btn-light btn-lg" href="https://www.facebook.com/profile.php?id=100011041895069"><i class="fab fa-facebook-f"></i></a>
-                        </li>
-                        <li class="list-inline-item">
-                            <!-- Git -->
-                            <a href="https://github.com/tranthuyvy">
-                                <img src="https://img.shields.io/github/followers/tranthuyvy?label=Follow&style=social" alt="GitHub Follow" />
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <!-- Twitter -->
-                            <a type="button" class="btn btn-floating btn-light btn-lg" href="#"><i class="fab fa-twitter"></i></a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a type="button" class="btn btn-floating btn-light btn-lg" href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
-                        </li>
-                        <li class="list-inline-item">
-                            <span><i class="fas fa-home"></i></span><span class="ms-2"> 97 Man Thien</span>
-                        </li>
-                        <li class="list-inline-item">
-                            <span><i class="fas fa-envelope"></i></span><span class="ms-2"> ttv.thuyvy.1544@gmail.com</span>
-                        </li>
-                        <li class="list-inline-item">
-                            <span><i class="fas fa-phone"></i></span><span class="ms-2"> 0373162586</span>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 text-right">
-                    2023 <a href="#">NHASACHTV</a> All Rights Reserved.
-                </div>
-            </div>
-        </div>
-    </footer>
+        <?= footerWidget::widget() ?>
     <!-- Footer END -->
 
     <?php $this->endBody() ?>
-
     </body>
     </html>
 <?php $this->endPage();
